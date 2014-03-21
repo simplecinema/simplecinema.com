933d48a4b6e4b2a8bd8f21c74c3cf295d82a5167
caiguanhao <caiguanhao@gmail.com>
2014-03-21 14:05:55 +0800

    adding workplace section;

--- assets/js/application.js
+++ assets/js/application.js
@@ -181,6 +181,26 @@ $(document).ready(function(){
         interval: 7000,
         pause: "hover"
     });
+
+    /* Initialize Slider */
+    var swiper = jQuery('#swiper').swiper({
+        loop:true,
+        grabCursor: true,
+        autoPlay: 4000
+    });
+
+    /* On Load swiper height should adjust to img size */
+    jQuery('.swiper-slide img').load(function() {
+        jQuery('#swiper').height(jQuery('.swiper-slide img').height());
+        jQuery('.swiper-wrapper').height(jQuery('.swiper-slide img').height());
+    });
+
+    /* On Resize swiper height should adjust to img size */
+    jQuery(window).resize(function() {
+        jQuery('#swiper').height(jQuery('.swiper-slide img').height());
+        jQuery('.swiper-wrapper').height(jQuery('.swiper-slide img').height());
+    });
+
 });
 
 
